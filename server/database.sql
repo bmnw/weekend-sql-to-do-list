a797db217d75636bf0735f74d92c5932ff89169d
Bryn Nadziejka Waller <bryn.m.nw@gmail.com>
2022-08-13 20:29:08 -0500

    updated database.sql with added table columns

--- server/database.sql
+++ server/database.sql
@@ -2,9 +2,12 @@
 CREATE TABLE tasks (
 	"id" serial PRIMARY KEY,
 	"task_description" varchar(200),
-	"complete" boolean,
+	"complete" boolean DEFAULT false,
 	"date_submitted" date,
-	"due_date" date
+	"due_date" date,
+	"priority" varchar(10),
+	"todays_date" date DEFAULT CURRENT_DATE;
+	"overdue" boolean DEFAULT false
 );
 
 -- insert a new task
